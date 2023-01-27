018751d35ee85bdc1106837df216ff5affce3115
Anjana19JK <anjanajoseph199@gmail.com>
2023-01-28 00:26:51 +0530

    Update index.js

--- src/components/TimelineView/index.js
+++ src/components/TimelineView/index.js
@@ -4,13 +4,29 @@ import CourseTimelineCard from '../CourseTimelineCard'
 
 const TimelineView = props => {
   const {timelineItemsList} = props
+  const {categoryId} = timelineItemsList
+  console.log(timelineItemsList)
 
   return (
     <div>
       <h1>MY JOURNEY OF CCBP 4.0</h1>
       <Chrono mode="VERTICAL_ALTERNATING" items={timelineItemsList} />
+      {timelineItemsList.map(eachItem =>
+        categoryId === 'COURSE' ? (
+          <CourseTimelineCard
+            key={eachItem.id}
+            timelineItemsList={timelineItemsList}
+          />
+        ) : (
+          <ProjectTimelineCard
+            key={eachItem.id}
+            timelineItemsList={timelineItemsList}
+          />
+        ),
+      )}
     </div>
   )
 }
 
 export default TimelineView
+
